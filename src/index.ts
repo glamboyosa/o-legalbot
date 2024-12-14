b1bf8ed104ff2bf5c6341bc3a2fee7065b260d06
glamboyosa <ogbemudiatimothy@gmail.com>
2024-12-14 18:54:59 +0100

    feat: get session string

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
-import { TelegramClient } from "telegram";
-import { StringSession } from "telegram/sessions";
 import client from "./utils";
 
 async function initializeTelegramClient() {
  
   await client.start({
     phoneNumber: async () =>  Bun.env.PHONE_NUMBER as string,
-    password: async () =>  prompt('Please enter your password: ') as string,
     phoneCode: async () =>  prompt('Please enter the code you received: ') as string,
     onError: (err) => console.log(err),
   });
